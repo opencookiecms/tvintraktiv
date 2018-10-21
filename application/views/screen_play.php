0d7d0f9a57a21a8b19b7439b3cac0afaa033d18b
Alyya Izzati <33283754+alyyaizzati@users.noreply.github.com>
2018-10-21 11:47:37 +0800

    slideshow

--- application/views/screen_play.php
+++ application/views/screen_play.php
@@ -49,6 +49,76 @@
      width: 100%;
      height: 350px;
    }
+
+   * {box-sizing: border-box;}
+   body {font-family: Verdana, sans-serif;}
+   .mySlides {display: none;}
+   img {vertical-align: middle;}
+
+   /* Slideshow container */
+   .slideshow-container {
+    max-width: 1000px;
+    position: relative;
+    margin: auto;
+  }
+
+  /* Caption text */
+  .text {
+    color: #f2f2f2;
+    font-size: 15px;
+    padding: 8px 12px;
+    position: absolute;
+    bottom: 8px;
+    width: 100%;
+    text-align: center;
+  }
+
+  /* Number text (1/3 etc) */
+  .numbertext {
+    color: #f2f2f2;
+    font-size: 12px;
+    padding: 8px 12px;
+    position: absolute;
+    top: 0;
+  }
+
+  /* The dots/bullets/indicators */
+  .dot {
+    height: 15px;
+    width: 15px;
+    margin: 0 2px;
+    background-color: #bbb;
+    border-radius: 50%;
+    display: inline-block;
+    transition: background-color 0.6s ease;
+  }
+
+  .active {
+    background-color: #717171;
+  }
+
+  /* Fading animation */
+  .fade {
+    -webkit-animation-name: fade;
+    -webkit-animation-duration: 1.5s;
+    animation-name: fade;
+    animation-duration: 1.5s;
+  }
+
+  @-webkit-keyframes fade {
+    from {opacity: .4} 
+    to {opacity: 1}
+  }
+
+  @keyframes fade {
+    from {opacity: .4} 
+    to {opacity: 1}
+  }
+
+  /* On smaller screens, decrease text size */
+  @media only screen and (max-width: 300px) {
+    .text {font-size: 11px}
+  }
   </style>
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
@@ -58,7 +128,7 @@
 
       <nav class="navbar navbar-static-top" style="margin-left:0px">
         <marquee behavior="scroll">
-          <h1>Selamat datang ke Jabatan Pengairan dan Saliran Negeri Kedah Darul Aman</h1>
+          <h1>Selamat datang ke Jabatan Pengairan dan Saliran Daerah Kuala Muda</h1>
         </marquee
 
         </nav>
@@ -88,7 +158,7 @@
 
               <!-- /.nav-tabs-custom -->
 
-              <div class="box box-solid bg-light-blue-gradient" style="height:200px">
+              <div class="box box-solid bg-green" style="height:200px">
                 <center>
                   <?php
                   $i = 1;
@@ -100,7 +170,6 @@
 
                    <tr>
                    <br>
-                    <td><h3>NEW MESSAGE</h3></td>
                     <td><?php echo $row->banner_title?></td>
                     <br>
                     <td><?php echo $row->banner_content?></td>
@@ -128,37 +197,50 @@
 
                 <div class="box-body">
 
+                  <div class="slideshow-container">
 
-                  <div id="demo" class="carousel slide" data-ride="carousel">
-
-                    <!-- Indicators -->
-                    <ul class="carousel-indicators">
-                      <li data-target="#demo" data-slide-to="1" class="active"></li>
-                      <li data-target="#demo" data-slide-to="2"></li>
-                      <li data-target="#demo" data-slide-to="3"></li>
-                    </ul>
-
-                    <!-- The slideshow -->
-                    <div class="carousel-inner">
-                      <div class="carousel-item active">
-                        <img src="<?php echo base_url('/assets/images/slide-1.jpg');?>" alt="Los Angeles" width="1100" height="500">
-                      </div>
-                      <div class="carousel-item">
-                        <img src="<?php echo base_url('/assets/images/slide-2.jpg');?>" alt="Chicago" width="1100" height="500">
-                      </div>
-                      <div class="carousel-item">
-                        <img src="<?php echo base_url('/assets/images/slide-3.jpg');?>" alt="New York" width="1100" height="500">
-                      </div>
+                    <div class="mySlides fade">
+                      <img src="<?php echo base_url('/assets/images/slide-1.jpg');?>" style="width:100%">
+                    </div>
+                    
+                    <div class="mySlides fade">
+                      <img src="<?php echo base_url('/assets/images/slide-3.jpg');?>" style="width:100%">
                     </div>
 
-                    <!-- Left and right controls -->
-                    <a class="carousel-control-prev" href="#demo" data-slide="prev">
-                      <span class="carousel-control-prev-icon"></span>
-                    </a>
-                    <a class="carousel-control-next" href="#demo" data-slide="next">
-                      <span class="carousel-control-next-icon"></span>
-                    </a>
+                    <div class="mySlides fade">
+                      <img src="<?php echo base_url('/assets/images/try.jpg');?>" style="width:100%">
+                    </div>
+
                   </div>
+                  <br>
+
+                  <div style="text-align:center">
+                    <span class="dot"></span> 
+                    <span class="dot"></span> 
+                    <span class="dot"></span> 
+                  </div>
+
+                  <script>
+                    var slideIndex = 0;
+                    showSlides();
+
+                    function showSlides() {
+                      var i;
+                      var slides = document.getElementsByClassName("mySlides");
+                      var dots = document.getElementsByClassName("dot");
+                      for (i = 0; i < slides.length; i++) {
+                       slides[i].style.display = "none";  
+                     }
+                     slideIndex++;
+                     if (slideIndex > slides.length) {slideIndex = 1}    
+                      for (i = 0; i < dots.length; i++) {
+                        dots[i].className = dots[i].className.replace(" active", "");
+                      }
+                      slides[slideIndex-1].style.display = "block";  
+                      dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+  }
+</script>
 
 
 
