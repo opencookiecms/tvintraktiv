445cc48deca69c1c13a24ffac775a917475e930f
Alyya Izzati <33283754+alyyaizzati@users.noreply.github.com>
2018-11-28 11:07:21 +0800

    Update screen_play02.php

--- application/views/photo.php
+++ application/views/photo.php
@@ -100,7 +100,7 @@
                  <tr>
                   <td><?php echo $row->id?></td>
                   <td><?php echo $row->slide_title?></td>
-                  <td><img src="<?php echo site_url("/assets/images/")?><?php echo $row->slide_content?>"></td>
+                  <td><img src="<?php echo site_url("/assets/images/")?><?php echo $row->slide_content?>" style="width: 30%"></td>
                   <td><?php echo $row->slide_status?></td>
                   <td><a href="<?php echo site_url("welcome/deletephoto/" . $row->id); ?>" onclick="return confirm('Are you sure?')">Delete</a>
                   </td>
---
+++ application/views/screen_play04.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.2/moment.min.js"></script>
+
+  <link href="<?php echo base_url();?>assets/style/css/block.css" rel="stylesheet" type="text/css">
+  <link href="<?php echo base_url();?>assets/style/css/ourthemes.css" rel="stylesheet" type="text/css">
+  <script src="<?php echo base_url();?>assets/style/js/bootstrap.min.css"></script>
+
+  <title>Screen cast 04</title>
+
+  <script type="text/javascript">
+    $('.carousel').carousel({
+      interval: 5000
+    })
+  </script>
+
+</head>
+<body>
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-3 whiteboxlogo headsize">
+      <div class="imageposition">
+        <img src="<?php echo base_url('/assets/logo/jps.png');?>">
+      </div>
+    </div>
+    <div class="col-md-9 whiteboxcontent headsize"><span class="fonthead">JABATAN PENGAIRAN SALIRAN KUALA MUDA \ SIK \ BALING</span>
+    </div>
+  </div>
+
+  
+  </div>
+
+
+</body>
+
+</html>
