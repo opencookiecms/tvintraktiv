a62be60f8564d65a0a7fead8b74a8d24290bef0e
opencookiecms <syed.m.afiq@outlook.com>
2019-03-01 11:32:16 +0800

    t

---
+++ application/views/screen6.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="<?php echo base_url();?>assets/owncss/screen5.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="container">
+    <div class="tv-header">tv-header</div>
+    <div class="tv-notice">tv-notice</div>
+    <div class="tv-vedio">tv-vedio</div>
+    <div class="tv-slide">tv-slide</div>
+    <div class="tv-banner">tv-banner</div>
+    <div class="tv-clock">tv-clock</div>
+    <div class="tv-massagebot">tv-massagebot</div>
+</div>
+
+</body>
+</html>
