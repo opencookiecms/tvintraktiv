38f0050f14fa7115e8ef1c6cd866238dc46b1fc7
SYED MOHD AFIQ SYED ABDUL RAHMAN <syed.m.afiq@outlook.com>
2019-05-21 03:51:46 -0700

    Update screen_play01.php

--- application/views/screen/screen_play01.php
+++ application/views/screen/screen_play01.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="<?php echo base_url();?>assets/owncss/newscreen01.css" rel="stylesheet" type="text/css">
-
+<!--https://grid.layoutit.com/?id=aQDSipO-->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
@@ -105,7 +105,11 @@ function initialize(){
 
 
 
-  <div class="banner">banner</div>
+  <div class="banner">
+    <div class="bannerlayourt">
+      <h1>This is banner</h1>
+    </div>
+  </div>
 </div>
 
 
